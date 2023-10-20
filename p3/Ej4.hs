67b968a2f130ecb1b4056c8967d4c76b15a979ac
rodrigo-vazq21 <72084466+rodrigo-vazq21@users.noreply.github.com>
2023-10-20 16:46:43 -0300

    ej4/p3

--- p3/Ej4.hs
+++ p3/Ej4.hs
@@ -1,5 +1,9 @@
-module Ej4 where
+module Main where
     import Parser
+    import Control.Monad.Reader
+    import Control.Monad.Except
+    import Control.Monad.Identity
+    import System.Environment
 
     data Expr = Let VName Expr Expr
       | Add Expr Expr
@@ -9,7 +13,7 @@ module Ej4 where
       deriving Show
     type VName = String
 
-    -- Parser de Expresiones
+    -- Parser de Expresiones (a)
     parserEXPR :: Parser Expr
     parserEXPR = do pSym 'l'
                     pSym 'e'
@@ -41,3 +45,54 @@ module Ej4 where
                 <|>
                 do  w <- word
                     return (Var w)
+
+    -- Definición del intérprete (b)
+    data Error = ErrorDivZero | ErrorUnbound VName
+        deriving Show
+    type Env = [(VName,Int)]
+
+    interp :: (MonadReader Env m,MonadError Error m) => Expr -> m Int
+    interp (Let var e1 e2) = do
+        val <- interp e1
+        local ((:) (var, val)) (interp e2)
+
+    interp (Add e1 e2) = do
+        val1 <- interp e1
+        val2 <- interp e2
+        return (val1 + val2)
+
+    interp (Div e1 e2) = do
+        val1 <- interp e1
+        val2 <- interp e2
+        if val2 == 0
+            then throwError ErrorDivZero
+            else return (val1 `div` val2)
+
+    interp (Num n) = return n
+
+    interp (Var var) = do
+        env <- ask
+        case lookup var env of
+            Just val -> return val
+            Nothing  -> throwError (ErrorUnbound var)
+
+    -- Evaluador (c)
+    type M a = ExceptT Error (ReaderT Env Identity) a
+
+    evalE :: Expr -> Either Error Int
+    evalE expr = runIdentity (runReaderT (runExceptT (interp expr :: M Int)) [])
+
+    -- Main
+    main :: IO ()
+    main = do
+        args <- getArgs
+        if null args 
+            then putStrLn "No se proporcionaron argumentos" 
+            else do
+            let fileName = head args
+            str <- readFile fileName
+            let exp = fst $ head $ (runP parserEXPR) str
+            let result = evalE exp
+            case result of
+                Left error -> putStrLn $ "Error: " ++ show error
+                Right value -> putStrLn $ "Resultado: " ++ show value
